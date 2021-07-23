7fbaf7c8655c7a614591051c141be647c3d288b4
MurrayWarnock <murray.warnock@gmail.com>
2021-07-22 18:56:24 -0600

    commit to test codegrade repository transfer

--- src/components/BubblePage.test.js
+++ src/components/BubblePage.test.js
@@ -5,7 +5,6 @@ import { render, screen} from "@testing-library/react";
 import BubblePage from './BubblePage';
 
 test("Renders without errors", ()=> {
-    
 });
 
 test("Renders appropriate number of colors passed in through mock", async ()=> {
